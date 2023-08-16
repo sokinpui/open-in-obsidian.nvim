3d8a53ccf1c8c749436b9ac4e3c18c57b9477735
SoKinPui <sosokinpui@gmail.com>
2023-08-16 19:38:07 +0800

    add support for mac and linux

--- lua/obs/init.lua
+++ lua/obs/init.lua
@@ -8,7 +8,11 @@ M.open_in_obsidian = function()
   -- temp path
   -- relative_path = "second_brain/resources/the_happy_sleeper.md"
 
-  local cmd = "silent !xdg-open \"obsidian://open?vault=" .. vault .. "&file=" .. relative_path .. "\"&> /tmp/xdg_open.log"
+  if vim.fn.has("mac") then
+    local cmd = "silent !open \"obsidian://open?vault=" .. vault .. "&file=" .. relative_path .. "\"&> /tmp/open.log"
+  elseif vim.fn.has("linux") then
+    local cmd = "silent !xdg-open \"obsidian://open?vault=" .. vault .. "&file=" .. relative_path .. "\"&> /tmp/xdg_open.log"
+  end
   vim.api.nvim_command(cmd)
 end
 
