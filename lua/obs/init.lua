db6059584168d5ca4aa94975123f2f5d93354ed8
SoKinPui <sosokinpui@gmail.com>
2023-08-16 17:22:42 +0800

    change to xdg-open

--- lua/obs/init.lua
+++ lua/obs/init.lua
@@ -8,7 +8,7 @@ M.open_in_obsidian = function()
   -- temp path
   -- relative_path = "second_brain/resources/the_happy_sleeper.md"
 
-  local cmd = "!open \"obsidian://open?vault=" .. vault .. "&file=" .. relative_path .. "\""
+  local cmd = "!xdg-open \"obsidian://open?vault=" .. vault .. "&file=" .. relative_path .. "\""
   vim.api.nvim_command(cmd)
 end
 
